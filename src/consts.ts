2d31181af70675f5599fa65bd69affa5034501a1
Deen Weible <weibledh@gmail.com>
2025-01-12 19:39:51 -0700

    Finish blog page, update header bar.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Weible Web!';
+export const SITE_TITLE = 'Weible Web';
 export const SITE_DESCRIPTION = "My website where I put stuff I'm working on/intrested in and a little ol' blog";
