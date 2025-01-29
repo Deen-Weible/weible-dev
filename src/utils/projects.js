acff644476b4b80f9b2cc01823fc7799f1254910
cardcraft <weibledh@gmail.com>
2025-01-28 19:54:24 -0700

    Refactor PageLayout and remove deleted Minigolf Mania page

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,18 +1,13 @@
 const projects = [
   {
-    title: "Mini Golf Mania",
-    description: "A phyics based mini-golf game where you challenge your friends online!",
-    link: "minigolf-mania"
+    title: "Stranger Things LEDS",
+    description: "An Arduino project to control a Stranger Things themed LED strip.",
+    link: "https://github.com/Deen-Weible/Stranger-Things-Leds",
+    emoji: "💡"
   }
 ];
 
 // Function to get projects
 export function getProjects() {
-  // add /project/ to each project's link
-  const updatedProjects = projects.map((project) => ({
-    ...project,
-    link: `/project/${project.link}`
-  }));
-
-  return updatedProjects;
+  return projects;
 }
